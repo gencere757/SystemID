09ff9b1aa07b9318e6d764680697bc1d74362ce9
gencere757 <arda.gencer@sabanciuniv.edu>
2026-07-09 15:49:19 +0300

    Added automatic feature selection

---
+++ NARX_Input_ID_Time_Series_Methods.m
@@ -0,0 +1,63 @@
+clc; clear; close all;
+
+load twotankdata;   %Load the data
+
+%Find state lag 
+max_lag = 25;
+
+%Compute and plot the partial autocorrelation function of output of system
+figure;
+[acf_vals, lags_pacf] = parcorr(y, max_lag);
+parcorr(y, max_lag);
+title('Autocorrelation of Output State');
+xlabel('Lags');
+ylabel('Sample Autocorrelation');
+
+%Clip the autocorrelation at the point x = 0 as its always 1
+acf_vals(lags_pacf == 0) = [];
+lags_pacf(lags_pacf == 0) = [];
+
+[~, sort_idx] = sort(abs(acf_vals), 'descend'); %Sort the correlations in descending manner
+top_5_output_lags = lags_pacf(sort_idx(1:min(5, length(sort_idx))));    %Take the 5 max points
+
+%Finding Input Lag
+
+%Differentiate the signals
+u_diff = diff(u);
+y_diff = diff(y);
+
+figure;
+max_lag = 100;  % Check cross-correlation up to 100 
+%Compute and plot the cross correlation between input and output
+[cc_vals, lags_cc] = crosscorr(u_diff, y_diff, max_lag);
+crosscorr(u_diff, y_diff, max_lag);
+title('Cross-Correlation between Input and Output (Differentiated)');
+xlim([0, max_lag]);
+
+%Clip the negative part of data
+positive_lags_idx = lags_cc >= 0;
+cc_vals_pos = cc_vals(positive_lags_idx);
+lags_cc_pos = lags_cc(positive_lags_idx);
+
+%Create confidence interval
+N = length(u_diff);
+confidence_threshold = 2 / sqrt(N); 
+
+% Find the first positive lag where correlation breaks above the threshold
+significant_lags = lags_cc_pos(abs(cc_vals_pos) > confidence_threshold);
+
+if ~isempty(significant_lags)
+    dead_time = significant_lags(1); % First significant lag = Dead Time
+else
+    dead_time = 0; % Default to 0 if no lag breaks the confidence threshold
+end
+
+%Read the values from cross correlation graph and the the interval where
+%cross correlation is confidently above the confidence threshold
+input_lags = lags_cc_pos(4:80);
+
+%The undifferentiated cross correlation signals
+% figure;
+% crosscorr(u, y, max_lag); 
+% title('Cross-Correlation between Input and Output');
+% xlim([0, max_lag]);
